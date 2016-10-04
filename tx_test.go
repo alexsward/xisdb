859863795571ddecd282bd6d3b697d6e83971abe
Alex Sward <asward@whittl.com>
2016-10-03 20:28:28 -0500

    Another unit test

--- tx_test.go
+++ tx_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 )
 
+// TestSetRollback -- tests an error creating a value
 func TestSetRollback(t *testing.T) {
 	fmt.Println("TestSetRollBack")
 
@@ -37,7 +38,43 @@ func TestSetRollback(t *testing.T) {
 	}
 }
 
-// testDeleteRollback -- tests rolling back a delete, transaction throws exception
+// TestSetUpdateRollback -- tests an error on update, gets rolled back
+func TestSetUpdateRollback(t *testing.T) {
+	fmt.Println("TestSetUpdateRollback")
+
+	db, _ := Open()
+	db.ReadWrite(func(tx *Tx) error {
+		return tx.Set("key", "value")
+	})
+
+	err := db.ReadWrite(func(tx *Tx) error {
+		tx.Set("key", "updatedValue")
+		return errors.New("Nope!")
+	})
+
+	if err != nil {
+		t.Error("There should have been an error thrown")
+	}
+
+	err = db.Read(func(tx *Tx) error {
+		val, err := tx.Get("key")
+		if err != nil {
+			return err
+		}
+
+		if val != "value" {
+			t.Error("Value was supposed to be value")
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		t.Error("There should have been an error thrown")
+	}
+}
+
+// TestDeleteRollback -- tests rolling back a delete, transaction throws exception
 func TestDeleteRollback(t *testing.T) {
 	fmt.Println("TestDeleteRollback")
 
